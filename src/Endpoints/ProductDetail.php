3a62d67974e0345f5d33cea6dbd2a528df69b5d9
tech-duomai <73681781+tech-duomai@users.noreply.github.com>
2021-04-02 17:53:33 +0800

    Update ProductDetail.php

--- src/Endpoints/ProductDetail.php
+++ src/Endpoints/ProductDetail.php
@@ -37,7 +37,7 @@ class ProductDetail extends EndpointBase
 
     public function Service()
     {
-        return "cps-mesh.cpslink.$this->platform.products.get";
+        return "cps-mesh.cpslink.$this->platform.products.detail";
     }
 
     public function getResult()
